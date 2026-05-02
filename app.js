0da32ce5823b7a262c8f3fd24b6a0674d9c5aaf1
taoufikx-star <taoufikbelamri55@gmail.com>
2026-05-02 12:39:10 +0100

    Créer des Données de Test

---
+++ app.js
@@ -0,0 +1,43 @@
+const testUsers = [
+    {
+        id: 1,
+        login: "torvalds",
+        name: "Linus Torvalds",
+        avatar_url: "https://avatars.githubusercontent.com/u/1024588?v=4",
+        bio: "Linux creator",
+        followers: 200000,
+        following: 0,
+        public_repos: 50
+    },
+    {
+        id: 2,
+        login: "gvanrossum",
+        name: "Guido van Rossum",
+        avatar_url: "https://avatars.githubusercontent.com/u/6490553?v=4",
+        bio: "Python creator",
+        followers: 50000,
+        following: 50,
+        public_repos: 30
+    }
+];
+
+// Repositories de test
+
+const testRepos = [
+    {
+        name: "linux",
+        description: "Linux kernel",
+        language: "C",
+        stargazers_count: 15000,
+        forks_count: 2000,
+        html_url: "https://github.com/torvalds/linux"
+    },
+    {
+        name: "cpython",
+        description: "Python interpreter",
+        language: "C",
+        stargazers_count: 50000,
+        forks_count: 23000,
+        html_url: "https://github.com/python/cpython"
+    }
+];
